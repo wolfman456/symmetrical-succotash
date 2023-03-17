8143cca2e4ca68c1d7a336956c08e7591bcace62
wolfman456 <pshook456@gmail.com>
2023-03-16 23:28:02 -0500

    update working

--- todoFront/main.js
+++ todoFront/main.js
@@ -1,4 +1,16 @@
-function getToDos() {
+
+let forms =
+`<form>
+  <input type="text" id="toDoName" placeholder="name">
+  <input type="text" id="des" placeholder="Description">
+  <input type="text" id="complete" placeholder= Completed>
+  <button type="button" name="button" onclick="saveToDo(toDoName, des, complete)">save</button>
+  <button type="button" onclick="location.reload()">Clear</button>
+</form>`
+document.getElementById("form").innerHTML = forms;
+let todolist = [];
+
+
   fetch("http://localhost:9092/todo")
     .then((response) => {
       if (response.ok) {
@@ -9,14 +21,14 @@ function getToDos() {
     })
     .then(data => {
       console.log(data);
+      todolist = data;
+      console.log(todolist);
       show(data)
     })
     .catch((error) => console.error("FETCH ERROR:", error));
-}
+
 function show(data) {
-  console.log("show")
   let todolist = data;
-  console.log(todolist[0].name)
   let tab =
     `<tr>
           <th>ID</th>
@@ -25,51 +37,48 @@ function show(data) {
           <th>complete</th>
           <th>Action</Action>
          </tr>`;
-
+         let i = 1;
   // Loop to access all rows 
   for (let r of todolist) {
     tab += `<tr>
-    <td>${r.id}</td>     
-    <td><input placeholder=${r.name}> </td>
-    <td><input placeholder=${r.description}></td>
-    <td><input placeholder=${r.complete}></td>
-    <td><button type="button">update</button>
-    <button type="button" onclick="updateItem(td)">Delete</button></td>      
+    <td <input>${r.id}</td>     
+    <td><input id="${"nameTag"+i}">${r.name}</td>
+    <td><input id="${"desTag"+i}">${r.description}</td>
+    <td><input id=${"statusTag"+i}>${r.complete}</td>
+    <td><button type="button" onclick="updateItem(${i})"> update</button>
+    <button type="button" onclick="deleteToDo(${i})">Delete</button></td>      
 </tr>`;
+i++;
   }
   document.getElementById("todos").innerHTML = tab;
-
-  let forms =
-    `<form>
-      <input type="text" id="toDoName" placeholder="name">
-      <input type="text" id="des" placeholder="Description">
-      <input type="text" id="complete" placeholder= Completed>
-      <button type="button" name="button" onclick="saveToDo(name, des, complete)">save</button>
-      <button type="button">Clear</button>
-    </form>`
-  document.getElementById("form").innerHTML = forms;
 
 }
 function saveToDo() {
   let name = document.getElementById("toDoName").value;
+  console.log(name);
   let des = document.getElementById("des").value;
   let complete = document.getElementById("complete").value;
-  if (name == null | des == null) {
-    name.innerHTML = "required";
-    des.innerHTML = "required";
-    return;
-
-  } else if (complete != false || complete != true || complete == null) {
+  if(name==""){
+    alert("Name required");
+    location.reload();
+  }
+  if (des == "") {
+    
+  }
+  if(complete == "false" || complete == null){
+    complete = false;
+    console.log(complete);
+  }
+  if(complete = "true"){
     complete = false;
   }
-  console.log(name, des, complete);
 
   fetch("http://localhost:9092/todo/create", {
   method: "POST",
   body: JSON.stringify({
     name: name,
     description: des,
-    completed: complete
+    complete: complete
   }),
   headers: {
     "Content-type": "application/json; charset=UTF-8"
@@ -83,14 +92,40 @@ function saveToDo() {
 })
 .then(data => {
   console.log(data);
-  return getToDos();
+  return location.reload();
 })
 .catch((error) => console.error("FETCH ERROR:", error));
 }
 
 
-function updateItem(td) {
-  console.log(td);
-}
+function updateItem(i) {
+  let nameTag = document.getElementById(`${"nameTag"+ i}`).value;
+  let desTag = document.getElementById(`${"desTag"+ i}`).value;
+  let statusTag = document.getElementById(`${"statusTag"+ i}`).value
+  let id = todolist[i-1].id;
 
-getToDos();
+  fetch("http://localhost:9092/todo/update", {
+    method: "POST",
+    body: JSON.stringify({
+      id:id,
+      name: nameTag,
+      description: desTag,
+      complete: statusTag
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    return location.reload();
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+  }
+
